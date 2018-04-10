e846faa317985a011165c7be772ae67b8e998d39
Shottah <m.abrahamtt@gmail.com>
2018-04-10 01:38:54 -0400

    cleaned urls.py

--- AnimeProj/AnimeApp/urls.py
+++ AnimeProj/AnimeApp/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls import url,include 
-from .views import index, update 
+from . import views
 
 
 
 urlpatterns=[
-    url(r'^listings', index),
-    #url(r'^detail', detail),
-    url(r'^update/(?P<anime_id>[0-9]+)$', update),
-    # path('', index),
-    # path('records', RecordView.as_view())
+    url(r'^listings', views.index),
+    url(r'^update/(?P<anime_id>[0-9]+)$', views.update),
+    url(r'^upload/', views.upload)
 ]
-   # path('newanime', index),
-  #  path('records', RecordView.as_view())
 
 
 
