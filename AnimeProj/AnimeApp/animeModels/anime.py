d42339fc2cfa7136b7e72410b85a5605775cd71b
Shottah <m.abrahamtt@gmail.com>
2018-04-10 01:41:32 -0400

    Anime.saveall() now handle_upload

--- AnimeProj/AnimeApp/animeModels/anime.py
+++ AnimeProj/AnimeApp/animeModels/anime.py
@@ -1,5 +1,6 @@
 import csv
 import uuid
+from .csv import ParseFile
 from django.db import models
 from django.db.models import (
 UUIDField,
@@ -9,7 +10,7 @@ BooleanField,
 DateTimeField,
 DecimalField
 )
-from .csv import ParseFile
+
 
 
 class Anime(models.Model):
@@ -20,24 +21,6 @@ class Anime(models.Model):
     Episodes= CharField(max_length=100)
     Ratings = CharField(max_length=100)  
     Members= CharField(max_length=100)  
-
-
-    @staticmethod 
-    def  readAll():
-         listings= []
-         AnimeObj=[]
-         listings =ParseFile.csvToDict() 
-         counter = 1
-         for data in listings:
-             #print (data)
-             #put into anime objects
-             
-             current= Anime(data['ID'], data['Name'],data['Genre'],data['Type'],data['Episodes'],data['Ratings'],data['Members'])
-             current.save()
-             counter = counter + 1
-             print(current.Name)
-         print("DONE")
-         return listings
-
-    # def __str__(self):
-    #     return Anime.Name
+    
+    def __str__(self):
+        return this.Name
